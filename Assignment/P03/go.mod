2ed8dfc952fff1807310d94d0a2fe3036c82fbb3
Sudhir0228 <raysudhir30@gmail.com>
2023-10-11 13:44:57 -0500

    done

--- Assignment/P03/go.mod
+++ Assignment/P03/go.mod
@@ -3,14 +3,8 @@ module P03
 go 1.21.0
 
 require (
-	github.com/Sudhir0228/Img_colors v0.0.0-20231006030331-3bfc4a8b4336
-	github.com/Sudhir0228/Img_get_pic v0.0.0-20231006024623-4b2392340cf3
-	github.com/Sudhir0228/Img_gray_scale v0.0.0-20231006032726-8eb019455658
-	github.com/Sudhir0228/Img_colors_text v0.0.0-20231006031609-a4b056b3a1d5
-)
-
-require (
+	github.com/Sudhir0228/mymodules v0.0.0-20231011182455-bb5779dfb444 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	golang.org/x/image v0.12.0 // indirect
+	golang.org/x/image v0.13.0 // indirect
 )
--- Assignment/P03/main.go
+++ Assignment/P03/main.go
@@ -1,1 +1,49 @@
-Stuck here!!!!
+// Sudhir Ray
+// PLC-4143
+// Program 3
+package main
+
+import (
+	"fmt"
+
+	"github.com/Sudhir0228/mymodules"
+)
+
+func main() {
+	imageURL := "https://s3e8p5g8.rocketcdn.me/wp-content/uploads/2020/11/midwestern-state-university2.jpg" // Replace with your actual image URL
+	fileName := "downloaded_image.jpg"                                                                      // Specify the desired file name
+
+	//will download a image file using its url
+	err := mymodules.GetImage(imageURL, fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	text := "Midwestern State University"
+	outputPath := "output.png"
+
+	//will print out colored text to a output file
+	err = mymodules.PrintColor(text, outputPath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Image saved as", outputPath)
+
+	err = mymodules.Pixels(outputPath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	output2 := "graymidwestern.png"
+
+	err = mymodules.Grayscale(fileName, output2)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+}
